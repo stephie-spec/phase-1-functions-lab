efa4b5c7afebfd7d583e497f1c20848f811ccf74
stephie-spec <compwork254@gmail.com>
2025-09-05 09:31:20 +0300

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+  if (x >= 42) {
+    return x - 42;
+  } else {
+    return 42 - x;
+  }
+}
+
+function distanceFromHqInFeet(x){
+    if (x >= 42) {
+    return (x - 42)*264;
+  } else {
+    return (42 - x)*264;
+  }
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start >= destination) {
+    return (start - destination) * 264;
+  } else {
+    return (destination - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance > 2500) {
+    return "cannot travel that far";
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return (distance - 400) * 0.02;
+  } else {
+    return 0;
+  }
+}
